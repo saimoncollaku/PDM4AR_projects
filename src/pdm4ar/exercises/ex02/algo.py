b67cc8a1a70502004abe6de161fcd6dbda60bbcf
SaimonC <155955381+saimoncollaku@users.noreply.github.com>
2024-09-25 12:56:25 +0000

    Trying to make update for ex 02

--- src/pdm4ar/exercises/ex02/algo.py
+++ src/pdm4ar/exercises/ex02/algo.py
@@ -1,5 +1,8 @@
 from abc import abstractmethod, ABC
 
+from networkx import reconstruct_path
+from sklearn import neighbors
+
 from pdm4ar.exercises.ex02.structures import AdjacencyList, X, Path, OpenedNodes
 
 
@@ -17,7 +20,28 @@ class GraphSearch(ABC):
 
 class DepthFirst(GraphSearch):
     def search(self, graph: AdjacencyList, start: X, goal: X) -> tuple[Path, OpenedNodes]:
-        # todo implement here your solution
+        # ! DOING
+        # Declaration
+        queued = []
+        visited = []
+        parents = []
+        
+        # Init
+        queued.append(start)
+        visited.append(start)
+        parents.append(None)
+        
+        while queued:
+            current = queued.pop()
+            if current == goal:
+                # reconstruct path
+                pass
+            else:
+                adjacents = list(AdjacencyList.get(current, set()))
+                
+
+                
+            
         return [], []
 
 
@@ -31,3 +55,8 @@ class IterativeDeepening(GraphSearch):
     def search(self, graph: AdjacencyList, start: X, goal: X) -> tuple[Path, OpenedNodes]:
         # todo implement here your solution
         return [], []
+    
+    
+def get_adjacent_nodes(adjacency_list: Mapping[X, Set[X]], node: X) -> List[X]:
+    adjacent_set = adjacency_list.get(node)
+    return list(adjacent_set) if adjacent_set is not None else []
