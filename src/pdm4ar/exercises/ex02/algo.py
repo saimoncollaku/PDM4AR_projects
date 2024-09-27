5c287cd01ea1e5f5b957de019fa5fbd7962ec70e
SaimonC <155955381+saimoncollaku@users.noreply.github.com>
2024-09-27 13:24:19 +0000

    Depth First to improve

--- src/pdm4ar/exercises/ex02/algo.py
+++ src/pdm4ar/exercises/ex02/algo.py
@@ -1,4 +1,7 @@
 from abc import abstractmethod, ABC
+from ast import List
+
+# from networkx import reconstruct_path
 
 from pdm4ar.exercises.ex02.structures import AdjacencyList, X, Path, OpenedNodes
 
@@ -17,8 +20,34 @@ class GraphSearch(ABC):
 
 class DepthFirst(GraphSearch):
     def search(self, graph: AdjacencyList, start: X, goal: X) -> tuple[Path, OpenedNodes]:
-        # todo implement here your solution
-        return [], []
+        # Init
+        queued: list[X] = [start]
+        visited: list[X] = [start]
+        opened: list[X] = []
+        parents = []
+        parents.append(None)
+
+        while queued:
+            # Remove first item in queue
+            current = queued.pop(0)
+            opened.append(current)
+
+            # Goal state
+            if current == goal:
+                path = construct(visited, parents)
+                return path, opened
+
+            # Other state
+            to_queue: list[X] = []
+            for adjacent in graph.get(current, set()):
+                if adjacent not in visited:
+                    visited.append(adjacent)
+                    to_queue.append(adjacent)
+                    parents.append(current)
+            queued = to_queue + queued
+
+        # Failed
+        return [], opened
 
 
 class BreadthFirst(GraphSearch):
@@ -31,3 +60,20 @@ class IterativeDeepening(GraphSearch):
     def search(self, graph: AdjacencyList, start: X, goal: X) -> tuple[Path, OpenedNodes]:
         # todo implement here your solution
         return [], []
+
+
+def construct(visited_nodes: list[X], parent_nodes: list[X]) -> list[X]:
+    # Last of the visited nodes is the goal, start from there
+    last_state = visited_nodes[-1]
+    parent_state = parent_nodes[-1]
+    path = [last_state]
+
+    # Stop when the parent is empty (reached the start)
+    while parent_state is not None:
+        last_state = parent_state
+        parent_state = parent_nodes[visited_nodes.index(last_state)]
+        path.append(last_state)
+
+    path.reverse()
+
+    return path
