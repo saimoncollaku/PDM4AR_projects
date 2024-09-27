a2697538c8f27114d12067637d614263b6fd5ad8
SaimonC <155955381+saimoncollaku@users.noreply.github.com>
2024-09-27 17:37:34 +0000

    [submit][02] ex 02 try 01

--- src/pdm4ar/exercises/ex02/algo.py
+++ src/pdm4ar/exercises/ex02/algo.py
@@ -43,7 +43,7 @@ class DepthFirst(GraphSearch):
                 if adjacent not in parents:
                     to_queue.append(adjacent)
                     parents[adjacent] = current
-            queue = to_queue + queue
+            queue = deque(sorted(to_queue)) + queue
 
         # Failed
         return [], opened
@@ -67,10 +67,12 @@ class BreadthFirst(GraphSearch):
                 return path, opened
 
             # Other state
+            to_queue = deque([])
             for adjacent in graph.get(current, set()):
                 if adjacent not in parents:
-                    queue.append(adjacent)
+                    to_queue.append(adjacent)
                     parents[adjacent] = current
+            queue = queue + deque(sorted(to_queue))
 
         # Failed
         return [], opened
@@ -78,8 +80,42 @@ class BreadthFirst(GraphSearch):
 
 class IterativeDeepening(GraphSearch):
     def search(self, graph: AdjacencyList, start: X, goal: X) -> tuple[Path, OpenedNodes]:
-        # todo implement here your solution
-        return [], []
+
+        d = 0
+        while True:
+            # * DFS with iteration on depth
+            d += 1
+
+            # Init
+            queue = deque([start])
+            opened: list[X] = []
+            parents: Dict[X, X | None] = {start: None}
+            depths: Dict[X, int] = {start: 1}
+
+            while queue:
+                # Remove first item in queue
+                current = queue.popleft()
+                opened.append(current)
+
+                # Goal state
+                if current == goal:
+                    path = reconstruct_path(parents, start, goal)
+                    return path, opened
+
+                # Other state
+                to_queue = deque([])
+                if depths[current] < d:
+                    for adjacent in graph.get(current, set()):
+                        if adjacent not in parents:
+                            to_queue.append(adjacent)
+                            parents[adjacent] = current
+                            depths[adjacent] = depths[current] + 1
+                    queue = deque(sorted(to_queue)) + queue
+
+            # Checks if the depth provided new states
+            if d not in depths.values():
+                # No path found
+                return [], opened
 
 
 def reconstruct_path(parents: Dict[X, Optional[X]], start: X, goal: X) -> Path:
